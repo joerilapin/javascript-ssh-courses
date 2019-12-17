7fee7885aa2d125ce8df0766ccbb06bc1a6c11d7
joerilapin <joerilapin>
2019-12-17 21:26:22 +0100

    kwadraat en afdruk op het scherm

--- javascript-complete-course/02-maths/03-even-square/script.js
+++ javascript-complete-course/02-maths/03-even-square/script.js
@@ -8,13 +8,41 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+/*
+(function() {
 
+   document.getElementById("run").addEventListener("click", function() {
+
+        // your code here
+       let varKwadraat;
+        for (var i = 1: i<22;i ++){
+             varKwadraat===i*i;
+            window.print (varKwadraat);
+        }
+
+    });
+9
+})();
+*/
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
+        var square;
+/* de lus gaat door tot het kwadraat van een getal tussen 1 en 21 niet groter is dan 21*/
+        
+        for (i =1; i*i < 21; i ++){
+
+            square = i*i;
+
+/*ik voorzie een spatie opdat de getallen niet tegen mekaar zouden plakken*/
+/*wat zou ik moeten schrijven als code om de kwadraten onder mekaar te zetten?---> writeln*??? die doet het niet??*/
+
+            document.writeln(square + " ");
+
+        }
 
     });
 
 })();
+
