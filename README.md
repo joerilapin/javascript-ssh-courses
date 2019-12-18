11f2fdc826c7bf8a0aa6dd4b335909773790e15b
joerilapin <joerilapin>
2019-12-18 12:32:54 +0100

    push

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # javascript-ssh-courses
 
+What do I expect from this course? (Everybody says it is difficult)
+HTML is the skeleton, CSS the skin, and Javascript is the musscles. I am very curious and exited, since now input from the screen is going to change the outcome on the screen!!!!
+
+
+
 checking ssh works
 
 finished with exercise 3
--- javascript-complete-course/03-colors/01-change-bcg-one/script.js
+++ javascript-complete-course/03-colors/01-change-bcg-one/script.js
@@ -12,5 +12,17 @@
 (function() {
 
     // your code here
+    document.getElementById("red").addEventListener("click", function(){
+        document.body.style.backgroundColor = "red";
+    })
+    document.getElementById("green").addEventListener("click", function(){
+        document.body.style.backgroundColor = "green";
+    })
+    document.getElementById("yellow").addEventListener("click", function(){
+        document.body.style.backgroundColor = "yellow";
+    })
+    document.getElementById("blue").addEventListener("click", function(){
+        document.body.style.backgroundColor = "blue";
+    })
 
 })();
