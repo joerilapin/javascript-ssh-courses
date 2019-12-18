437b21b2dbb655aab40703ca4dc2d87969c02d7f
joerilapin <joerilapin>
2019-12-18 11:27:35 +0100

    I have put function factorial outside the loop

--- javascript-complete-course/02-maths/05-factorial/script.js
+++ javascript-complete-course/02-maths/05-factorial/script.js
@@ -9,14 +9,38 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
     
     // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
+
 
         // your code here
+        document.getElementById("run").addEventListener("click", function() {
+
+            let input = document.getElementById('number').value;
+
+            let number = parseInt(input);
+
+            let result = factorial(number);
+
+            document.write(result);
+
+
 
     });
 
 })();
+
+function factorial(number){
+    if (number > 0 && number <= 1){
+
+        return 1;
+    }
+
+    else {
+
+        return number * factorial(number-1);
+    }
+
+}
