a30cbc356967ec97464fd42c2751e11bc830d14e
joerilapin <joerilapin>
2019-12-19 10:27:33 +0100

    basis if then else

--- javascript-complete-course/04-dates/01-title-by-hour-one/script.js
+++ javascript-complete-course/04-dates/01-title-by-hour-one/script.js
@@ -9,29 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later .
 
-(function() {
+//(function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
 
-/*het referentie-uur is 18h00, als het huidige uur  <18h00, dan document.write  hello
-                                                    >18h00, dan                 good evening
- */
+/*GOOGLE-LOG
+ //hoe haal ik de tijd op? -->https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript
+ // Set variable to current date and time
+        var todaysDate = new Date();
+        var actualHour = todaysDate.getHours();
+*/
 
 if actualHour<18h00{
-    document.write("Hello")
+    "Hello")
     };
     else{
-    document.write("good evening");
+   "good evening");
     }
 
-    /*hoe haal ik de tijd op? -->https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript
 
-// Set variable to current date and time
-const now = new Date();
 
-// View the output
-now;
 
 })();
