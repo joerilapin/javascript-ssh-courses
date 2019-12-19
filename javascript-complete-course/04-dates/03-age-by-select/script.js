28cc94397b3477cb88257c752e98c4b20cfdfe3f
joerilapin <joerilapin>
2019-12-19 14:18:59 +0100

    het bepalen van de leeftijf in een if...else

--- javascript-complete-course/04-dates/03-age-by-select/script.js
+++ javascript-complete-course/04-dates/03-age-by-select/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function()
+{
 
     // your code here
 
+
+//de leeftijdsbepaling:
+        let age = year -birthyear;
+        if ((month >=birthMonth) || ((month = birthmonth) && (day >=birthDay))) )
+        {alert("You are " + age + " years old ;)");"years old :)"
+        }
+        else
+        {
+         alert("you are " + (age - 1) +)
+        }
 })();
