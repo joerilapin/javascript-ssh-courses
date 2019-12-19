decbe4aecc54b450d3663334f5f819eb2e44257c
joerilapin <joerilapin>
2019-12-19 08:59:59 +0100

    09h attendancy

--- javascript-complete-course/04-dates/01-title-by-hour-one/script.js
+++ javascript-complete-course/04-dates/01-title-by-hour-one/script.js
@@ -7,12 +7,13 @@
  */
 
 // NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+// You will have time to focus on it later .
 
 (function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    ss
 
 })();
