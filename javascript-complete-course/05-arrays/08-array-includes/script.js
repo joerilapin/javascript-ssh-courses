6c3bb2cc2b0ad027b4788d2c45a22a14a1e597e0
joerilapin <joerilapin>
2019-12-30 14:32:02 +0100

    5.8

--- javascript-complete-course/05-arrays/08-array-includes/script.js
+++ javascript-complete-course/05-arrays/08-array-includes/script.js
@@ -27,5 +27,12 @@
     ];
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        if (fruits.includes("apple")) {
+            return console.log("yes");
+        } else {
+            return console.log("no");
+        }
 
+    })
 })();
