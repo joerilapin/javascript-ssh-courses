b942c14eb9aa5e4b0c989da68623c42d7ec935bf
joerilapin <joerilapin>
2019-12-31 11:22:59 +0100

    6.4

--- javascript-complete-course/06-dom/04-attr-create/script.js
+++ javascript-complete-course/06-dom/04-attr-create/script.js
@@ -11,6 +11,16 @@
 
 (function() {
 
-    // your code here
+     //your code here
+    //haal uit het element span de waarde van het atribuut met naam data-image en steek het in de variabele image
+    var image = document.getElementsByTagName("span")[1].getAttribute("data-image");
+    //creeer een element met naam img en ken eht toe aan de variabele met naam elementImage
+    var elementImage = document.createElement("img");
+    //ken de variabele image toe aan de variabele elementImage zijn attribuut src
+    elementImage.src = image;
+    //adding the img through making child of elementImage to the element with id="target"
+    document.getElementById("target").appendChild(elementImage);
+    //removing the original element
+    document.getElementById("source").remove();
 
 })();
