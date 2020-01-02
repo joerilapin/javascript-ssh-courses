518c7738e7c3b50c80c71696cccf03282aa69f39
joerilapin <joerilapin>
2020-01-02 15:08:55 +0100

    6.6

--- javascript-complete-course/06-dom/06-mini-carousel/script.js
+++ javascript-complete-course/06-dom/06-mini-carousel/script.js
@@ -18,7 +18,26 @@
         "../../_shared/img/lemon.svg",
         "../../_shared/img/map.svg",
     ];
-
+    console.log(gallery);
     // your code here
+
+        //  let imgUsed = document.getElementsByTagName("img")[0];
+        // console.log(imgUsed);
+    //  take the existing gallery array as a starting point since the 'bell' is the first
+    //  element in it
+        let i = 0;
+    document.getElementById("next").addEventListener("click", function () {
+        console.log(gallery[i]);
+        if (i < 5) {
+//schrijf in de HTML aan de hand van de queryselector het ide element uit de gallery
+// array mbv setattribute tot i=5, dan wordt i=0 en is de cirkel rond
+            document.querySelector("img").setAttribute("src", gallery[i] );
+            i++;
+
+        }
+        //
+        else {i = 0}
+
+    });
 
 })();
