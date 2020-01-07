4c16a916e54942bfe4267a8a4029f8d3f5b38828
joerilapin <joerilapin>
2020-01-07 12:12:29 +0100

    backtracking

--- javascript-complete-course/05-arrays/02-manipulate-array/script.js
+++ javascript-complete-course/05-arrays/02-manipulate-array/script.js
@@ -25,9 +25,11 @@
     ];
 
     // your code here
-        fruits.shift();//'shift' removes the first element of the array, and changes the lengt of the array
-        fruits.pop();//'pop' remove the last element of the array and changes the length of the array
-        fruits.unshift("banana");//Javascript array unshift() is an inbuilt function that adds one or more items or elements to the beginning of the array and returns the new length of the array. The unshift() method changes the length of the array directly, so it is not a pure function
+        fruits.shift();//'shift' removes the first element of the array, and changes the length of the array
+        fruits.pop();//'pop' removes the last element of the array and changes the length of the array
+        fruits.unshift("banana");//Javascript array unshift() is an inbuilt function that adds one or more items
+                    // or elements to the beginning of the array and returns the new length of the array. The unshift() method
+                    // changes the length of the array directly, so it is not a pure function
         fruits.push("kiwi");//The push() method adds new items to the end of an array, and returns the new length.
 
     alert(fruits);
